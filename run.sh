d2e6dc680f6a2174dc7c9a3cd9286fd96588cac4
Orlovsky Maxim <orlovskiy.maxim@huawei-partners.com>
2023-09-25 17:54:32 +0300

    Fixed run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,17 @@
+antlr_v=antlr-4.13.1-complete.jar
+
+export CLASSPATH=".:/usr/local/lib/${antlr_v}:$CLASSPATH"
+
+function antlr4 { 
+    java -Xmx500M -cp "/usr/local/lib/${antlr_v}:$CLASSPATH" org.antlr.v4.Tool "$@"
+}
+export -f antlr4
+
+function grun { 
+    java org.antlr.v4.gui.TestRig "$@"
+}
+export -f grun
+
 antlr4 TypeScriptLexer.g4 TypeScriptParser.g4
 javac -cp ".:/usr/local/lib/antlr-4.13.1-complete.jar" *.java
 echo Running:
